ce160f1c3ccb15f9193e6791afa8c257a51e02e9
Joshua <jrtbing@gmail.com>
2025-09-24 00:07:46 +0100

    Now functional

--- initial_setup.py
+++ initial_setup.py
@@ -15,8 +15,15 @@ def get_api_key():
         try:
             api_key = input("Please enter your gemini API key: ")
             with open(".env", "w") as environment:
-                environment.write(f"API_KEY = \"{api_key}\"")
-                pass
-            return api_key
+                environment.write(f"GEMINI_API_KEY = \"{api_key}\"")
+                #return api_key
         except:
             raise FileNotFoundError("There was an error writing the API_Keys, please manually create the environment file")
+
+
+#Getting queries
+with open("query_fiction.txt") as file:
+  query_fiction = file.read()
+
+with open("query_non_fiction.txt") as file:
+  query_non_fiction = file.read()

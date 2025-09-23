36661d04752a69abc88867835e3473362739df6e
j <121765426@users.noreply.github.com>
2025-09-23 13:55:47 +0100

    added imports

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
+#Imports required for gemini access
 from google import genai
+from google.genai import types
+#For tcl/tk access
+from tkinter import *
+from tkinter import tkk
+
+#Imports from project
+from initial_setup import get_api_key
 
 
-from google import genai
+
+api_key = get_api_key()
+def get_ebook():
+  pass
+  
 
 # The client gets the API key from the environment variable `GEMINI_API_KEY`.
-client = genai.Client()
-
+client = genai.Client(api_key = api_key)
+"""
 response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="Explain how AI works in a few words"
+    model="gemini-2.5-flash",
+    contents="Please explain how I'm using the gemini api",
+    config = types.GenerateContentConfig(
+      thinking_config=types.ThinkingConfig(thinking_budget=0) #Disables thinking
+    )
 )
 print(response.text)
+"""

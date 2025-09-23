b2ef4935c80abadb71514cf0dfdac2bf75f3b2bc
Joshua <jrtbing@gmail.com>
2025-09-23 18:04:02 +0100

    Started functions for converting epub to str

--- ebook_reader.py
+++ ebook_reader.py
@@ -1,8 +1,31 @@
 import ebooklib
+from bs4 import BeautifulSoup
+import tkinter
+import tkinter.filedialog
 from ebooklib import epub
 
-selected_book = None
-book = epub.read_epub(selected_book)
+def epub_to_html(file_path: str):
+    book = epub.read_epub(file_path)
+    chapters = []
+    #chapters = book.get_items_of_type(ebooklib.ITEM_DOCUMENT)
+    for item in book.get_items():
+        if item.get_type() == ebooklib.ITEM_DOCUMENT:
+            chapters.append(item.get_content())
+    return chapters
 
-for image in book.get_items_of_type(ebooklib.ITEM_IMAGE):
-    print(image)
+def chapter_to_str(chapter):
+    soup = BeautifulSoup(chapter.get_body_content(), 'html.parser')
+
+def chapter_to_str(chapter):
+    soup = BeautifulSoup(chapter.get_body_content(), 'html.parser')
+    text = [para.get_text() for para in soup.find_all('p')]
+    return ' '.join(text)
+
+
+book_path = tkinter.filedialog.askopenfilename()
+chapters = epub_to_html(book_path)
+print(chapters)
+
+#book = epub.read_epub(selected_book.name)
+#book_items = list(book.get_items_of_type(ebooklib.ITEM_DOCUMENT))
+#print(book_items)

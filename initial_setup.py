10469d2f475c9518282e191c83c9f91f6a53588d
Joshua <jrtbing@gmail.com>
2025-09-24 02:24:39 +0100

    added fucntion for initalising bookshelf

--- initial_setup.py
+++ initial_setup.py
@@ -1,9 +1,14 @@
 from dotenv import load_dotenv
 import os
 from pathlib import Path
+import jsons
+
+
+#Constants
 environment_file_path = Path(".env")
 
 
+#Functions
 def get_api_key():
     if environment_file_path.exists():
         load_dotenv()
@@ -21,9 +26,23 @@ def get_api_key():
             raise FileNotFoundError("There was an error writing the API_Keys, please manually create the environment file")
 
 
-#Getting queries
-with open("query_fiction.txt") as file:
-  query_fiction = file.read()
+#Loading files
+try:
+    with open("query_fiction.txt") as file:
+        query_fiction = file.read()
 
-with open("query_non_fiction.txt") as file:
-  query_non_fiction = file.read()
+    with open("query_non_fiction.txt") as file:
+        query_non_fiction = file.read()
+except FileNotFoundError:
+    raise FileNotFoundError("ERROR: could not find one of query_fiction.txt or query_non_fiction.txt.")
+
+def get_bookshelf():
+    try:
+        with open("bookshelf.json") as file:
+            bookshelf = jsons.load(file.read()) #This is causing issues. jsons doc gives more info on deserialisation. Need to find a way to seperate each book class.
+    except FileNotFoundError:
+        print("Bookshelf file missing, creating empty bookshelf. Ignore if this is the first run.")
+        bookshelf = []
+        with open("bookshelf.json", 'w') as file:
+            pass
+    return bookshelf
